ce5e3947210e8ec5d9e6bbf1639cfcc0b96e6ed2
jailsonbernardo <jailsonbernado@hotmail.com>
2017-04-10 07:55:32 -0500

    alteração de template

---
+++ WebSisDoar/rodape.php
@@ -0,0 +1,181 @@
+	
+        </div>
+		<!-- END CONTENT -->
+		
+		
+		
+		
+		
+		<!--
+		============================================================================
+		MODAL DIALOG EXAMPLE
+		You can change transition style, just view element page
+		============================================================================
+		-->
+		<!-- Modal Logout Primary -->
+		<div class="md-modal md-fall" id="logout-modal">
+			<div class="md-content">
+				<h3><strong>Logout</strong> Confirmation</h3>
+				<div>
+					<p class="text-center">Are you sure want to logout from this awesome system?</p>
+					<p class="text-center">
+					<button class="btn btn-danger md-close">Nope!</button>
+					<a href="login.html" class="btn btn-success md-close">Yeah, I'm sure</a>
+					</p>
+				</div>
+			</div>
+		</div><!-- End .md-modal -->
+		
+		<!-- Modal Logout Alternatif -->
+		<div class="md-modal md-just-me" id="logout-modal-alt">
+			<div class="md-content">
+				<h3><strong>Logout</strong> Confirmation</h3>
+				<div>
+					<p class="text-center">Are you sure want to logout from this awesome system?</p>
+					<p class="text-center">
+					<button class="btn btn-danger md-close">Nope!</button>
+					<a href="login.html" class="btn btn-success md-close">Yeah, I'm sure</a>
+					</p>
+				</div>
+			</div>
+		</div><!-- End .md-modal -->
+		
+		<!-- Modal Task Progress -->	
+		<div class="md-modal md-slide-stick-top" id="task-progress">
+			<div class="md-content">
+				<h3><strong>Task Progress</strong> Information</h3>
+				<div>
+					<p>CLEANING BUGS</p>
+					<div class="progress progress-xs for-modal">
+					  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+						<span class="sr-only">80&#37; Complete</span>
+					  </div>
+					</div>
+					<p>POSTING SOME STUFF</p>
+					<div class="progress progress-xs for-modal">
+					  <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 65%">
+						<span class="sr-only">65&#37; Complete</span>
+					  </div>
+					</div>
+					<p>BACKUP DATA FROM SERVER</p>
+					<div class="progress progress-xs for-modal">
+					  <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 95%">
+						<span class="sr-only">95&#37; Complete</span>
+					  </div>
+					</div>
+					<p>RE-DESIGNING WEB APPLICATION</p>
+					<div class="progress progress-xs for-modal">
+					  <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+						<span class="sr-only">100&#37; Complete</span>
+					  </div>
+					</div>
+					<p class="text-center">
+					<button class="btn btn-danger btn-sm md-close">Close</button>
+					</p>
+				</div>
+			</div>
+		</div><!-- End .md-modal -->
+		<!-- Modal New message -->	
+		<div class="md-modal md-slide-stick-top" id="new-message">
+			<div class="md-content">
+			<div class="md-close-btn"><a class="md-close"><i class="fa fa-times"></i></a></div>
+				<h3><strong>New</strong> Message</h3>
+				<div>
+					<form role="form">
+						<div class="form-group">
+							<input type="text" class="form-control input-lg" placeholder="Message to">
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control input-lg" placeholder="Message subject">
+						</div>
+						<div class="form-group">
+							<textarea class="summernote-small form-control"></textarea>
+						</div>
+						<div class="row">
+							<div class="col-xs-8">
+								<button type="submit" class="btn btn-success btn-sm">Send</button>
+								<button class="btn btn-warning btn-sm">Draft</button>
+							</div>
+							<div class="col-xs-4">
+								<p class="quick-post message">
+									<a><i class="fa fa-picture-o"></i></a>
+									<a><i class="fa fa-video-camera"></i></a>
+									<a><i class="fa fa-paperclip"></i></a>
+								</p>
+							</div>
+						</div>	
+					</form>
+				</div>
+			</div>
+		</div>
+		
+		<!--
+		============================================================================
+		END MODAL DIALOG EXAMPLE
+		============================================================================
+		-->
+		
+		<!--
+		MODAL OVERLAY
+		Always place this div at the end of the page content
+		-->
+		<div class="md-overlay"></div>
+		
+		
+		
+	</div><!-- End div .container -->
+	<!-- END PAGE -->
+
+	<!--
+	================================================
+	JAVASCRIPT
+	================================================
+	-->
+	<!-- Basic Javascripts (Jquery and bootstrap) -->
+	<script src="assets/js/jquery.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+	<!-- VENDOR -->
+	
+	<!-- Slimscroll js -->
+	<script src="assets/third/slimscroll/jquery.slimscroll.min.js"></script>
+	
+	<!-- Morris js -->
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+	<script src="assets/third/morris/morris.js"></script>
+	
+	<!-- Nifty modals js -->
+	<script src="assets/third/nifty-modal/js/classie.js"></script>
+	<script src="assets/third/nifty-modal/js/modalEffects.js"></script>
+	
+	<!-- Sortable js -->
+	<script src="assets/third/sortable/sortable.min.js"></script>
+	
+	<!-- Bootstrao selectpicker js -->
+	<script src="assets/third/select/bootstrap-select.min.js"></script>
+	
+	<!-- Summernote js -->
+	<script src="assets/third/summernote/summernote.js"></script>
+	
+	<!-- Magnific popup js -->
+	<script src="assets/third/magnific-popup/jquery.magnific-popup.min.js"></script> 
+	
+	<!-- Bootstrap file input js -->
+	<script src="assets/third/input/bootstrap.file-input.js"></script>
+	
+	<!-- Bootstrao datepicker js -->
+	<script src="assets/third/datepicker/js/bootstrap-datepicker.js"></script>
+	
+	<!-- Icheck js -->
+	<script src="assets/third/icheck/icheck.min.js"></script>
+	
+	<!-- Form wizard js -->
+	<script src="assets/third/wizard/jquery.snippet.min.js"></script>
+	<script src="assets/third/wizard/jquery.easyWizard.js"></script>
+	<script src="assets/third/wizard/scripts.js"></script>
+	
+	<!-- LANCENG TEMPLATE JAVASCRIPT -->
+	<script src="assets/js/lanceng.js"></script>
+
+	</body>
+</html>
