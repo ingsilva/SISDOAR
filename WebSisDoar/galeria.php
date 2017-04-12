b290e439c99b3ce4a084f0e3a806cf95a0035e43
francisca.ingryd <francisca.ingryd@WS005568.dominio.eletroacre>
2017-04-12 13:32:18 -0500

    Galeria modificada

---
+++ WebSisDoar/galeria.php
@@ -0,0 +1,87 @@
+<html>
+	<head>
+	
+        <meta charset="UTF-8">
+        <title>Galeria</title>
+        <link rel="icon" type="image/png"  href="assets/img/apple-touch-icon.png" />
+        <?php
+        include './layout/cabecalho.php';
+        ?>
+	</head>
+         <body class="tooltips">
+            <div class="box-info">
+                        <!-- Begin gallery with caption -->
+				<div class="box-info">
+                                    
+					<h2><strong>Gallery</strong> with caption</h2>
+					<!-- Begin gallery with caption -->
+					<div class="gallery-wrap">
+						<div class="column">
+							<div class="inner">
+								<a class="zooming" href="images/big/img001.jpg" title="Image title here">
+								<div class="img-wrap">
+									<img src="images/small/img001_small.jpg" alt="Image gallery" title="Image title here" class="mfp-fade">
+								</div>
+								<div class="caption-hover">
+								Hi, I'm a caption!
+								</div>
+								</a>
+							</div>
+						</div>
+						<div class="column">
+							<div class="inner">
+								<a class="zooming" href="images/big/img002.jpg" title="Image title here">
+								<div class="img-wrap">
+									<img src="images/small/img002_small.jpg" alt="Image gallery" title="Image title here" class="mfp-fade">
+								</div>
+								</a>
+								<div class="caption-hover success">
+								Hi, I'm a success caption!
+								</div>
+							</div>
+						</div>
+						<div class="column">
+							<div class="inner">
+								<a class="zooming" href="images/big/img003.jpg" title="Image title here">
+								<div class="img-wrap">
+									<img src="images/small/img003_small.jpg" alt="Image gallery" title="Image title here" class="mfp-fade">
+								</div>
+								<div class="caption-hover warning">
+								Hi, I'm a warning caption!
+								</div>
+								</a>
+							</div>
+						</div>
+						<div class="column">
+							<div class="inner">
+								<a class="zooming" href="images/big/img004.jpg" title="Image title here">
+								<div class="img-wrap">
+									<img src="images/small/img004_small.jpg" alt="Image gallery" title="Image title here" class="mfp-fade">
+								</div>
+								<div class="caption-hover danger">
+								Hi, I'm a danger caption!
+								</div>
+								</a>
+							</div>
+						</div>
+						<div class="column">
+							<div class="inner">
+								<a class="zooming" href="images/big/img005.jpg" title="Image title here">
+								<div class="img-wrap">
+									<img src="images/small/img005_small.jpg" alt="Image gallery" title="Image title here" class="mfp-fade">
+								</div>
+								<div class="caption-hover info">
+								Hi, I'm a info caption!
+								</div>
+								</a>
+							</div>
+						</div>
+					</div><!-- End div .gallery-wrap -->
+					<!-- End gallery with caption -->
+						
+						<hr />
+                                </div>
+            </div>
+
+        </body>
+</html>
