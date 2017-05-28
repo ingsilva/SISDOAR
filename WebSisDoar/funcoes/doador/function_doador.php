4f24b70a0b602872e5c4ff2861307be5d5c061b6
Laaah <Laaah@192.168.1.65>
2017-05-28 17:47:32 -0300

    Alterações feitas

--- WebSisDoar/funcoes/doador/function_doador.php
+++ WebSisDoar/funcoes/doador/function_doador.php
@@ -2,7 +2,12 @@
 
 include("../../config/conexao.php");
 $id = $_POST["id"];
+$data_registro = $_POST["data_registro"];
+$id_doador = $_POST["id_doador"];
+$numero_sus = $_POST["numero_sus"];
 $nome = $_POST["nome"];
+$nome_pai = $_POST["nome_pai"];
+$nome_mae = $_POST["nome_mae"];
 $data_nascimento = $_POST["data_nascimento"];
 $cpf = $_POST["cpf"];
 $rg = $_POST["rg"];
@@ -14,6 +19,9 @@ $cidade_idcidade = $_POST["cidade"];
 $tipo_sangue = $_POST["tipo_sangue"];
 $fator_rh = $_POST["fator_rh"];
 $idade = $_POST["idade"];
+$nacionalidade = $_POST["nacionalidade"];
+$naturalidade = $_POST["naturalidade"];
+$uf = $_POST["uf"];
 $sexo = $_POST["sexo"];
 
 if ($id == 0) {
@@ -22,7 +30,12 @@ if ($id == 0) {
             . "doador (nome, data_nascimento, cpf, rg, endereco, numero, "
             . "bairro, complemento, cidade_idcidade, tipo_sangue, fator_rh, idade, sexo)"
             . " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $sql->bindValue(1, $data_registro);
+    $sql->bindValue(1, $id_doador);
+    $sql->bindValue(1, $numero_sus);
     $sql->bindValue(1, $nome);
+    $sql->bindValue(1, $nome_pai);
+    $sql->bindValue(1, $nome_mae);
     $sql->bindValue(2, $data_nascimento);
     $sql->bindValue(3, $cpf);
     $sql->bindValue(4, $rg);
@@ -34,6 +47,9 @@ if ($id == 0) {
     $sql->bindValue(10, $tipo_sangue);
     $sql->bindValue(11, $fator_rh);
     $sql->bindValue(12, $idade);
+    $sql->bindValue(1, $nacionalidade);
+    $sql->bindValue(1, $naturalidade);
+    $sql->bindValue(1, $uf);
     $sql->bindValue(13, $sexo);
 
 
