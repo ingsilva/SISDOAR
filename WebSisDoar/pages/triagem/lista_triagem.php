fcb04b63b1ea45146b41fb28b8ded21ec90c30f7
saio_ <saio_@192.168.1.12>
2017-06-01 18:26:02 -0700

    Listagem completa

--- WebSisDoar/pages/triagem/lista_triagem.php
+++ WebSisDoar/pages/triagem/lista_triagem.php
@@ -1,16 +1,15 @@
 <?php
 include '../../config/conexao.php';
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Lista de Triagem</title>
-
+        <title>Triagem de Doadores</title>
         <?php
         include '../../layout/cabecalho.php';
         ?>
-
         <script type="text/javascript">
             $(document).ready(function () {
                 $('a#excluir').livequery("click", function () {
@@ -37,20 +36,18 @@ include '../../config/conexao.php';
             });
         </script>
     </head>
-    <body >
-
-
+    <body>
 
         <div class="body content rows scroll-y">
 
             <!-- Page header -->
             <div class="page-heading animated fadeInDownBig">
-                <h1> Triagem <small> de Doadores </small></h1>
+                <h1> Triagem <small>de Doadores</small></h1>
             </div>
             <!-- End page header -->
 
             <div class="table-responsive">
-
+                
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <div class="row">
@@ -59,7 +56,7 @@ include '../../config/conexao.php';
                     </div>
                     <!-- /.panel-heading -->
                     <div class="panel-body">
-                        <table width="100%" class="table table-striped table-bordered table-hover" id="example2">
+                        <table width="100%" class="table table-striped table-bordered table-hover" id="triagem">
                             <thead>
                                 <tr>
                                     <th>ID</th>
@@ -67,28 +64,28 @@ include '../../config/conexao.php';
                                     <th>Idade</th>
                                     <th>Peso</th>
                                     <th>Status</th>
-                                    <th>Ação</th>
+                                    <th>Editar</th>
+                                    <th>Excluir</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
-                                $sql = ("select idtriagem, nome, idade, peso, status,
-                                                case  status
-                                                when 'Apt' then 'Apto'
-                                                when 'NaoApt' then 'Não Apto'
-                                                end status
-                                        from doador d, triagem t
-                                            where t.doador_iddoador = d.iddoador;");
+                                $sql = ("select t.idtriagem, d.nome, t.idade, t.peso, t.status"
+                                        . " from doador d, triagem t"
+                                        . " where t.doador_iddoador = d.iddoador ");
                                 foreach ($con->query($sql) as $row) {
                                     ?>
                                     <tr>
                                         <td><?php echo $row['idtriagem']; ?></td>
                                         <td><?php echo $row['nome']; ?></td>
-                                        <td class="text-center"><?php echo $row['idade']; ?></td>
-                                        <td class="text-center"><?php echo $row['peso']; ?></td>
-                                        <td class="text-center"><?php echo $row['status']; ?></td>
+                                        <td><?php echo $row['idade']; ?></td>
+                                        <td><?php echo $row['peso']; ?></td>
+                                        <td><?php echo $row['status']; ?></td>
                                         <td>
-                                            <?php echo "<a class='btn btn-info' href='edit_user.php?id=" . $row['idtriagem'] . "'><i class='glyphicon glyphicon-plus'></i></a>"; ?>
+                                            <?php echo "<a class='btn btn-info' href='edit_user.php?id=" . $row['idtriagem'] . "'><i class='glyphicon glyphicon-edit'></i></a>"; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo "<a href='#' class='btn btn-danger' id='excluir' rel='" . $row['idtriagem'] . "'><i class='glyphicon glyphicon-remove'></i></a>"; ?>
                                         </td>
                                     </tr>
                                     <?php
@@ -102,13 +99,11 @@ include '../../config/conexao.php';
                     </div>
                     <!-- /.panel-body -->
                 </div>
-                <!-- /.panel -->
             </div>
-
 
             <script>
                 $(function () {
-                    $('#example2').DataTable({
+                    $('#table_cidade').DataTable({
                         "paging": true,
                         "lengthChange": true,
                         "searching": true,
@@ -128,3 +123,4 @@ include '../../config/conexao.php';
             ?>
     </body>
 </html>
+
